73319e5028521d631234a8486c70d1fbd32aefbf
Gbeminiyi 'Telo Briggs' Oshoba <gbeminiyi.oshoba@gmail.com>
2022-01-27 02:01:04 +0100

    remove logger from broker file

--- src/workers/broker.js
+++ src/workers/broker.js
@@ -1,14 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const lm = require('live-mutex');
-const path = require('path');
-const Logger = require(path.join(__dirname, '../../src/utils/logger.ts'));
-
-const logger = new Logger.LoggerService(__filename);
 
 Promise.all([new lm.Broker(Object.freeze({ port: 6972, host: '0.0.0.0' })).ensure()]).then(function ([b]) {
   b.emitter.on('warning', function () {
     // eslint-disable-next-line prefer-rest-params
-    logger.warn('BrokerError: ', ...arguments);
+    console.log('BrokerError: ', ...arguments);
   });
 });
