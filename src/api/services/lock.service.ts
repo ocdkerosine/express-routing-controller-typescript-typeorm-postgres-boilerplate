54657497fc6dfe02b897d82b35d9928afe2853b1
Gbeminiyi 'Telo Briggs' Oshoba <gbeminiyi.oshoba@gmail.com>
2022-01-27 02:26:55 +0100

    lock service update

--- src/api/services/lock.service.ts
+++ src/api/services/lock.service.ts
@@ -10,7 +10,7 @@ export class LockService {
   // be sure to use the docker cli and run "route" to obtain the IP address. it will change if you rebuild the container.
   // localhost is supposed to work. 0.0.0.0 is also supposed to work but if they dont then thats the workaround
   // ref - https://github.com/ORESoftware/live-mutex/issues/46
-  static lock = new Client({ port: 6972, host: '0.0.0.0', ttl: 11000, lockRetryMax: 0 });
+  static lock = new Client({ port: 6972, host: 'localhost', ttl: 11000, lockRetryMax: 0 });
 
   constructor(@Logger(__filename) private logger: LoggerService) {
     LockService.lock.emitter.on('warning', function () {
